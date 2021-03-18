5e54955043fa036343fa6e04e8c2a515aa7093dd
TheSekrestyes <70635260+TheSekrestyes@users.noreply.github.com>
2021-03-18 12:27:05 +0100

    Add files via upload

---
+++ szures.php
@@ -0,0 +1,46 @@
+<?php
+require_once "config.php";
+
+$sql="SELECT orszag FROM eredmeny";
+$stmt=$db->query($sql);
+$eredmeny="";
+$eredmeny2="";
+$tipus="";
+while($row=$stmt->fetch()){
+    extract($row);
+    $eredmeny.="<option value='{$orszag}'>{$orszag}</option>";
+}
+
+
+
+if(isset($_POST['btn'])){
+    $id=$_POST['id'];
+    $sql="SELECT a.nev,b.ev FROM versenyzo a, eredmeny b WHERE a.az=b.versenyzoaz AND b.orszag='{$id}' ORDER BY b.ev";
+    $stmt=$db->query($sql);
+
+    if($stmt->rowCount()==0){
+        $tipus="Nincs ilyen";
+    }
+    else{
+        while($row=$stmt->fetch()){
+            extract($row);
+            $eredmeny2.="<li>{$nev} - {$ev}</li>";
+        }
+    }
+    //amugy ez a Dani gyerek kurva idegesítő nem?
+    
+}
+?>
+
+<form method="post">
+    <div class="form-group">
+        <select name="id" class="custom-select mt-3 w-25">
+            <?=$eredmeny?>
+        </select>
+    </div>
+    <input type="submit" value="OK" name="btn" class="btn btn-dark">
+</form>
+
+<ul>
+    <?=$eredmeny2?>
+</ul>
